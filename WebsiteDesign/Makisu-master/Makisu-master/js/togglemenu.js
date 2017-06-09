c71830df30270f5d4019606b258e97b2fde2e0dc
Unknown <murugappanviswanathan@gmail.com>
2017-06-09 18:54:25 +0530

    Added about us page and other changes

---
+++ WebsiteDesign/Makisu-master/Makisu-master/js/togglemenu.js
@@ -0,0 +1,31 @@
+if ( $.fn.makisu.enabled ) {
+	var $nigiri = $( '.nigiri' );
+
+	// Create Makisus
+
+	$nigiri.makisu({
+		selector: 'dd',
+		overlap: 0.85,
+		speed: 1.7
+	});
+
+	// Open all
+	
+	$( '.list' ).makisu( 'open' );
+
+	// Toggle on click
+
+	$( '.toggle' ).on( 'click', function() {
+		$( '.list' ).makisu( 'toggle' );
+	});
+
+	// Disable all links
+
+	$( '.demo a' ).click( function( event ) {
+		event.preventDefault();
+	});
+
+} else {
+
+	$( '.warning' ).show();
+}
